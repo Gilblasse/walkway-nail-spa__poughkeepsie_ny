721aa33eb040383176f07204e9564d90fe0fd5e0
Nethelbert <Gilblasse@gmail.com>
2020-11-18 13:03:02 -0500

    added routes to specified components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
 import React from 'react';
 import './App.scss';
-import Hero from './components/Hero/Hero';
-import NavBar from './components/NavBar/NavBar'
+import { Switch, Route } from "react-router-dom";
+import Home from './containers/Home/Home'
+import AboutPage from './containers/About/AboutPage'
+import ServicesPage from './containers/ServicesPage/ServicesPage'
+import LookBookPage from './containers/LookBookPage/LookBookPage';
+import ContactPage from './containers/ContactPage/ContactPage'
+import BookingPage from './containers/BookingPage/BookingPage';
+// import 'mapbox-gl/dist/mapbox-gl.css';`
+
+
+
 
 
 function App() {
+ 
+
+
+
   return (
     <div className="App">
-      <NavBar/>
-      <Hero/>
-      
+
+      <Switch>
+        <Route exact path="/about" component={AboutPage}/>
+        <Route exact path="/services" component={ServicesPage}/>
+        <Route exact path="/lookbook" component={LookBookPage}/>
+        <Route exact path="/contact" component={ContactPage}/>
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/book-appointment" component={BookingPage}/>
+        {/* <Route path="*" component={()=> "404 Page Not Found"}/> */}
+      </Switch>
+
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.scss'
+import { BrowserRouter as Router} from "react-router-dom";
+import { Auth0Provider } from '@auth0/auth0-react';
 
-
-ReactDOM.render(<App />,  document.getElementById('root'));
+ReactDOM.render(
+    <Router>
+        <Auth0Provider
+            domain={process.env.REACT_APP_AUTH0_DOMAIN}
+            clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
+            redirectUri={window.location.origin}
+        >
+            <App />
+        </Auth0Provider>
+    </Router>,  
+    document.getElementById('root')
+);
